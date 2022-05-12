36b4f184ad09496af5ab439aed1be4a4c7601ccc
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-05-12 23:49:19 +0300

    fix popup error positions

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,32 +1,9 @@
 import React, { useContext } from 'react';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-// import api from '../../utils/Api';
 import Card from '../Card/Card';
 
 function Main ({onEditAvatar, onEditProfile, onAddPlace, onCardClick, onCardLike, onCardDelete, cards }) {
     const currentUser = useContext(CurrentUserContext);
-    // const [userAvatr, setUserAvatar] = useState('#');
-    // const [userName, setUserName] = useState('...');
-    // const [userDescription, setUserDescription] = useState('...');
-    // const [cards, setCards] = useState([]);
-
-    // useEffect(() => {
-    //     Promise.all([api.getProfile(), api.getInitialCards()])
-    //         .then(res => {
-    //             const getProfile = res[0];
-    //             const getInitialCards = res[1];
-
-    //             setUserName(getProfile.name)
-    //             setUserDescription(getProfile.about)
-    //             setUserAvatar(getProfile.avatar)
-
-    //             return getInitialCards;
-    //         })
-    //         .then(res => {
-    //             setCards(res.map(item => item))
-    //         })
-    //         .catch(err => console.err(`Error: ${err}`))
-    // }, [])
 
     return (
         <main className="main">
